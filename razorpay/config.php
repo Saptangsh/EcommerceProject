b032610d03242d56e0720733ded848f150de7118
Saptangshu Ghosh <saptangshu27ghosh@gmail.com>
2024-08-03 21:36:01 +0530

    first commit

---
+++ razorpay/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId ='rzp_test_k9OnbzkXuab5Ds';
+$keySecret = 'CvTtsDR3neWZz5QFsRJPEhHM';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
